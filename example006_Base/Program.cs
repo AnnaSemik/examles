d7764442498ba5d426b748ff23784e0f8aafd744
Anna <anna-c-91@mail.ru>
2023-10-16 21:01:36 +1200

    добавили решения задач

---
+++ example006_Base/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача с гирями
+*/
+
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+int max = a;
+if (b > max) b=max;
+if (c > max) c=max;
+if (d > max) d=max;
+if (e > max) e=max;
+
+
+
+
---
+++ example011/Program.cs
@@ -0,0 +1,30 @@
+﻿// Прописали 2 метода: 1 для заполнения массива, другой для печати его. 
+// Затем прописали наш массив, он будет состоять из 10 элементов.
+// вызвали методы - заполнили наш массив и распечатали.
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
